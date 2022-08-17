bc268156587dcfd9de0a8494f714240db6a64cf1
Kidrash <markkaranja11@gmail.com>
2022-08-18 00:03:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+//  const names = []
+//  function writeCards(name,){
+    // for(let i = 0; i < names.length; i++){
+        // names.push("Guadalupe", "Ollie", "Aki")
+    // };
+    // console.log(`Thank you, ${names[i]} for the wonderful surprise gift!`);
+// };
+
+//   <<The above code was a try out>> 
+//   <<I spent a lot of time here>>
+
+const cardReceivers = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    cardReceivers.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  };
+  return cardReceivers;
+};
+
+
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  };
+};
